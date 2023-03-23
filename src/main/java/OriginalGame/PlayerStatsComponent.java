a68598fd60c8f70387d9285c5cd036c108e23dfc
yilmazaj <yilmazaj@rose-hulman.edu>
2023-03-23 15:04:57 -0400

    Created methods for updating resource amounts in PlayerStats

--- src/main/java/OriginalGame/PlayerStatsComponent.java
+++ src/main/java/OriginalGame/PlayerStatsComponent.java
@@ -5,21 +5,38 @@ import java.awt.*;
 
 
 public class PlayerStatsComponent extends JFrame {
+    String playerName;
+    int victoryPoints;
+    int brickAmount;
+    int grainAmount;
+    int lumberAmount;
+    int woolAmount;
+    int oreAmount;
+    JFrame frame;
+    JPanel playerNamePanel, resourceDisplayPanel;
 
-    public static void main(String[] args) {
+    PlayerStatsComponent(String name, int vp, int brick, int grain, int lumber, int wool, int ore) {
 
-        JFrame frame = new JFrame("My Frame");
+        this.playerName = name;
+        this.victoryPoints = vp;
+        this.brickAmount = brick;
+        this.grainAmount = grain;
+        this.lumberAmount = lumber;
+        this.woolAmount = wool;
+        this.oreAmount = ore;
 
-        JPanel playerNamePanel = new JPanel(new GridLayout(1, 2));
-        JPanel resourceDisplayPanel = new JPanel(new GridLayout(5, 1));
+        frame = new JFrame("My Frame");
 
-        playerNamePanel.add(new JLabel("Player Name: playerName"));
-        playerNamePanel.add(new JLabel("VP: victoryPoints"));
-        resourceDisplayPanel.add(new JLabel("Brick: brickAmount"));
-        resourceDisplayPanel.add(new JLabel("Grain: grainAmount"));
-        resourceDisplayPanel.add(new JLabel("Lumber: lumberAmount"));
-        resourceDisplayPanel.add(new JLabel("Wool: woolAmount"));
-        resourceDisplayPanel.add(new JLabel("Ore: oreAmount"));
+        playerNamePanel = new JPanel(new GridLayout(1, 2));
+        resourceDisplayPanel = new JPanel(new GridLayout(5, 1));
+
+        playerNamePanel.add(new JLabel("Player Name: " + this.playerName));
+        playerNamePanel.add(new JLabel("VP: " + this.victoryPoints));
+        resourceDisplayPanel.add(new JLabel("Brick: " + this.brickAmount));
+        resourceDisplayPanel.add(new JLabel("Grain: " + this.grainAmount));
+        resourceDisplayPanel.add(new JLabel("Lumber: " + this.lumberAmount));
+        resourceDisplayPanel.add(new JLabel("Wool: " + this.woolAmount));
+        resourceDisplayPanel.add(new JLabel("Ore: " + this.oreAmount));
 
         frame.getContentPane().setLayout(new GridLayout(2, 1));
 
@@ -29,5 +46,28 @@ public class PlayerStatsComponent extends JFrame {
         frame.setSize(400, 300);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void updateResourceValues(int brick, int grain, int lumber, int wool, int ore){
+        this.brickAmount += brick;
+        this.grainAmount += grain;
+        this.lumberAmount += lumber;
+        this.woolAmount += wool;
+        this.oreAmount += ore;
+        updateResourcesView();
+    }
+
+    public void updateResourcesView(){
+        resourceDisplayPanel.removeAll();
+        resourceDisplayPanel.add(new JLabel("Brick: " + this.brickAmount));
+        resourceDisplayPanel.add(new JLabel("Grain: " + this.grainAmount));
+        resourceDisplayPanel.add(new JLabel("Lumber: " + this.lumberAmount));
+        resourceDisplayPanel.add(new JLabel("Wool: " + this.woolAmount));
+        resourceDisplayPanel.add(new JLabel("Ore: " + this.oreAmount));
+    }
+
+    public static void main(String[] args) {
+        PlayerStatsComponent p = new PlayerStatsComponent("Player", 0, 0, 0, 0, 0, 0);
+        p.updateResourceValues(0,1,2,3,4);
     }
 }
