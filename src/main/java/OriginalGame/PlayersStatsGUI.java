79f9fed138c08e70773eb0e8e9d6fd4bda3e95a2
yilmazaj <yilmazaj@rose-hulman.edu>
2023-03-30 00:44:23 -0400

    Adding PlayerStatsGUI files

---
+++ src/main/java/OriginalGame/PlayersStatsGUI.java
@@ -0,0 +1,39 @@
+package Team7.SettlersOfCatan;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayersStatsGUI extends JFrame {
+
+    public JFrame frame;
+    public PlayerStatsGUI[] playerStatsGUIs;
+    public Player[] players;
+
+    public PlayersStatsGUI(Player[] players) {
+
+        this.players = players;
+        this.playerStatsGUIs = new PlayerStatsGUI[players.length];
+
+        frame = new JFrame();
+        frame.getContentPane().setLayout(new GridLayout(players.length*2, 1));
+
+        for(int i = 0; i < players.length; i++){
+            playerStatsGUIs[i] = new PlayerStatsGUI(players[i]);
+            frame.add(playerStatsGUIs[i].playerNamePanel);
+            frame.add(playerStatsGUIs[i].resourceDisplayPanel);
+        }
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(300, 500);
+    }
+
+    public void updatePlayersStats() {
+        System.out.println("Updating player stats GUI");
+        for(int i = 0; i < players.length; i++){
+            playerStatsGUIs[i].updateResourcesView();
+        }
+        frame.revalidate();
+        frame.repaint();
+    }
+}
