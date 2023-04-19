694892e362c2378a3cdf7e1fc3dbe21bca8ca3ea
Andrew Kosikowski <kosikoaj@rose-hulman.edu>
2023-04-19 12:57:03 -0400

    More Large Game Class changes for Game Board

--- src/main/java/OriginalGame/Presentation/HexagonData.java
+++ src/main/java/OriginalGame/Presentation/HexagonData.java
@@ -1,15 +1,37 @@
 package Team7.SettlersOfCatan.Presentation;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class HexagonData {
     public int xCenter;
     public int yCenter;
     public Polygon hex;
+    
+    public final int HEX_SIDE_LENGTH = 70;
 
     public HexagonData(int x, int y, Polygon hexagon) {
         xCenter = x;
         yCenter = y;
+        hex = hexagon;
+    }
+
+    public HexagonData(Point2D center){
+        xCenter = (int) center.getX();
+        yCenter = (int) center.getY();
+
+        Polygon hexagon = new Polygon();
+        hexagon.addPoint(xCenter + 1, yCenter + HEX_SIDE_LENGTH + 1);
+        hexagon.addPoint(xCenter + (int) (HEX_SIDE_LENGTH * Math.sqrt(3 / 2)) + 1,
+                yCenter + (int) (.5 * HEX_SIDE_LENGTH) + 1);
+        hexagon.addPoint(xCenter + (int) (HEX_SIDE_LENGTH * Math.sqrt(3 / 2)) + 1,
+                yCenter - (int) (.5 * HEX_SIDE_LENGTH) - 1);
+        hexagon.addPoint(xCenter + 1, yCenter - HEX_SIDE_LENGTH - 1);
+        hexagon.addPoint(xCenter - (int) (HEX_SIDE_LENGTH * Math.sqrt(3 / 2)) - 1,
+                yCenter - (int) (.5 * HEX_SIDE_LENGTH) - 1);
+        hexagon.addPoint(xCenter - (int) (HEX_SIDE_LENGTH * Math.sqrt(3 / 2)) - 1,
+                yCenter + (int) (.5 * HEX_SIDE_LENGTH) + 1);
+
         hex = hexagon;
     }
 
@@ -19,6 +41,8 @@ public class HexagonData {
         g2.setStroke(new BasicStroke(5));
         g2.drawPolygon(hex);
     }
+
+
 
     public int getXCenter(){
         return xCenter;
