4559c484b8cdb432ac95fac2cc91ddfa3be5b1b7
yilmazaj <yilmazaj@rose-hulman.edu>
2023-04-19 23:23:56 -0400

    Pushing SpecialtyCard Test

---
+++ src/test/NewGame/SpecialtyCardTest.java
@@ -0,0 +1,20 @@
+import Team7.SettlersOfCatan.SpecialtyCard;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SpecialtyCardTest {
+
+    @Test
+    public void specialtyCardsCollapsedHierarchyTest(){
+        SpecialtyCard mostRoads = new SpecialtyCard("MostRoads");
+        SpecialtyCard largestArmy = new SpecialtyCard("LargestArmy");
+
+        assertEquals("MostRoads", mostRoads.getType());
+        assertEquals("LargestArmy", largestArmy.getType());
+
+        assertTrue(2 == mostRoads.getPointValue() && 2 == largestArmy.getPointValue());
+    }
+
+}
